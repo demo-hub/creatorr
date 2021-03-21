663f746ff985c2805dafc2deebc8be0ec53fe552
João Rodrigues <joao.rodrigues@inova.business>
2021-03-21 11:23:26 +0000

    prefill payment form

--- UI/imageCode.js
+++ UI/imageCode.js
@@ -95,7 +95,7 @@ function addImage(ctx, creatorrInfo) {
 }
 
 function exportImage() {
-    if (creatorr.cardRarityTitle == 'UR') {
+    if (creatorr.cardRarityTitle == 'UR' || creatorr.cardRarityTitle == 'SR') {
         const startit = () => {
             setTimeout(function() {
                 console.log("start");
@@ -288,7 +288,7 @@ function getDateString() {
 
 function getRandomRarity() {
     var num = Math.random();
-    if (num < 0.3) return rarity.ULTRARARE; //probability 0.3
+    if (num < 0.3) return rarity.SUPERRARE; //probability 0.3
     else if (num < 0.5) return rarity.UNCOMMON; // probability 0.2
     else if (num < 0.7) return rarity.RARE; //probability 0.2
     else if (num < 0.9) return rarity.SUPERRARE; //probability 0.2

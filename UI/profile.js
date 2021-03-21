ee193185e2ded2bf955457d529d37d553af38569
Andrew Rangel <andrew.rangel@qualapps.com>
2021-03-20 19:42:50 -0500

    initial commit for profile select

---
+++ UI/profile.js
@@ -0,0 +1,9 @@
+
+function getProfileBio(name) {
+  switch name {
+    case "Andrew":
+    return "Andrew loves posting on Instagram";
+    default:
+    return "This is a paragraph explaining how i spend my life, breathing, walking, and cashing like";
+  }
+}

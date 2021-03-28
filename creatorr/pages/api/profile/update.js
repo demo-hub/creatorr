6c42a9bc13dc8577006ad8783e97276d5e1463da
João Rodrigues <joao.rodrigues@inova.business>
2021-03-28 19:02:28 +0100

    change collection name

--- creatorr/pages/api/profile/update.js
+++ creatorr/pages/api/profile/update.js
@@ -2,6 +2,6 @@ import { connectToDatabase } from "../../../util/mongodb";
 
 export default async (req, res) => {
     const { db } = await connectToDatabase();
-    const updated = await db.collection('users').updateOne({ email: req.body.email }, { $set: req.body })
+    const updated = await db.collection('userUsers').updateOne({ email: req.body.email }, { $set: req.body })
     res.status(200).json(updated)
 }
